937921cc20916642641a69f664e5621fefdc9e79
KongMJ <kongmj.sjamwp@gmail.com>
2020-07-18 21:11:40 +0800

    logo done

--- Introduction to Computer Systems/Assignment/brand.asm
+++ Introduction to Computer Systems/Assignment/brand.asm
@@ -1,31 +1,33 @@
 .MODEL SMALL
 .STACK 100
 .DATA
-	WEL	DB 0DH, 0AH, "          **         **"
-		DB 0DH, 0AH, "          ****     ****"
-		DB 0DH, 0AH, "             **   **"
-		DB 0DH, 0AH, "              *****"
-		DB 0DH, 0AH, "               ***"
-		DB 0DH, 0AH, "          ||    *    ||"
-		DB 0DH, 0AH, "       ||  \  /***\  /   ||"
-		DB 0DH, 0AH, "        \   \ ----- /    /"
-		DB 0DH, 0AH, "         \   /  |  \    /"
-		DB 0DH, 0AH, "          \_/   |   \ _/"
-		DB 0DH, 0AH, "           |    |    |"
-		DB 0DH, 0AH, "         __|    |    |__"
-		DB 0DH, 0AH, "        /  \    |    /    \"
-		DB 0DH, 0AH, "       /    =========      \"
-		DB 0DH, 0AH, "      ||      =====        ||"
-		DB 0DH, 0AH, "==================================="
-		DB 0DH, 0AH, "|| Welcome to Royal Beetles Farm ||"
-		DB 0DH, 0AH, "===================================$"
+    LOGO    DB 0DH, 0AH, "  _________                                                     _________"
+            DB 0DH, 0AH, " /         \___                                             ___/         \"
+		    DB 0DH, 0AH, " |             \__                                       __/             |"
+		    DB 0DH, 0AH, "  \               \___                               ___/               /"
+	    	DB 0DH, 0AH, "   \                  \___       _       _       ___/                  /"
+	        DB 0DH, 0AH, "    \                     \_      \     /      _/                     /"
+	    	DB 0DH, 0AH, "     \                      \_     \   /     _/                      /"
+	    	DB 0DH, 0AH, "      \                       \_    \_/    _/                       /"
+	    	DB 0DH, 0AH, "       \_                       \__ (_) __/                       _/"
+	    	DB 0DH, 0AH, "         \__                       /   \_                      __/"
+	    	DB 0DH, 0AH, "            \_______        _____//\ _ /\\_______        _____/"
+	    	DB 0DH, 0AH, "               /    \______/     /  |_|  \       \______/  \"
+	    	DB 0DH, 0AH, "              /               __/   |_|   \__               \"
+	    	DB 0DH, 0AH, "              \            __/      |_|      \__            /"
+	    	DB 0DH, 0AH, "               \        __/         |_|         \__        /"
+	    	DB 0DH, 0AH, "                \______/            \-/            \______/"
+	    	DB 0DH, 0AH, "              ==============================================="
+	    	DB 0DH, 0AH, "              || Welcome to Queen Alexandra Butterfly Farm ||"
+	    	DB 0DH, 0AH, "              ===============================================$"
 
 .CODE
+MAIN PROC
 	MOV AX, @DATA
 	MOV DS, AX
 
 	MOV AH, 09H
-	LEA DX, WEL
+	LEA DX, LOGO
 	INT 21H
 
 	MOV AX, 4C00H
